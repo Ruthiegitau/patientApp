a1f5acea891c20e3c3e77aeec690dc51e7f21c87
Pato <ruthienjerigitau@gmail.com>
2017-05-22 17:24:05 +0300

    edit functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,21 +10,21 @@ export class AppComponent {
       new Patient("Moses Okemwa", "tyty","123456","d@ymail.com","1998-12-12")
   ]
 
-  savePatient(firstName:string,lastName:string,phoneNumbe:string,emailAddress:string,dateofBirth:string){
-    var newPatient = new Patient(firstName,lastName,phoneNumbe,emailAddress,dateofBirth);
+  savePatient(firstName:string,lastName:string,phoneNumber:string,emailAddress:string,dateofBirth:string){
+    var newPatient = new Patient(firstName,lastName,phoneNumber,emailAddress,dateofBirth);
 
     this.patients.push(newPatient);
   }
-  // selectedPatient: Patient = null;
-  // showDetails(clickedPatient: Patient) {
-  //   this.selectedPatient = clickedPatient;
-  // }
-  // finishedEditing() {
-  //   this.selectedPatient = null;
-  // }
+  selectedPatient: Patient = null;
+  showDetails(clickedPatient: Patient) {
+    this.selectedPatient = clickedPatient;
+  }
+  finishedEditing() {
+    this.selectedPatient = null;
+  }
 }
 
 export class Patient {
   public done: boolean = false;
-  constructor(public firstName: string, public lastName: string, public phoneNumbe: string, public emailAddress: string, public dateofBirth: string) {   }
+  constructor(public firstName: string, public lastName: string, public phoneNumber: string, public emailAddress: string, public dateofBirth: string) {   }
 }
